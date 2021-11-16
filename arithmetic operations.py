e65cee247c7282edd965ec8b3fe12c600a7272f5
sahalabakker <sahalabakker01@gmail.com>
2021-11-16 10:42:38 +0530

    first commit

---
+++ arithmetic operations.py
@@ -0,0 +1,8 @@
+a=7
+b=3
+sum=a+b
+print(sum)
+diff=a-b
+print(diff)
+pro=a*b
+print(pro)
---
+++ if-else.py
@@ -0,0 +1,6 @@
+number=3
+if number > 99 and number < 1000:
+    print('3 digit')
+else:
+    print('3 not digit')
+   

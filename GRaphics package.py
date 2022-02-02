0e1b3a55f5192c330502bf4b71dad374d8784bff
sahalabakker <94426450+sahalabakker@users.noreply.github.com>
2022-02-02 16:26:22 +0530

    Add files via upload

---
+++ GRaphics package.py
@@ -0,0 +1,13 @@
+from Graphics.RectFunctions import*
+from Graphics.CirFunctions import*
+from Graphics.DGraphics.SphereFunctions import*
+i=int(input("Enter r"))
+b=int(input("Enter b"))
+print("area=",RectArea(i,b))
+print("perimeter=",REctPerimetr(i,b))
+r=int(input("Enter radiuse of circle"))
+print("area of circle=",CirArea(r))
+print("perimeter of circle=",CirPeremter(r))
+r1=int(input("Enter radiuse of sphere"))
+print("Area of sphere=",SpArea(r1))
+print("perimeter of sphere=",SpPerimeter(r1))
